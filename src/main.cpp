22f71d79be5fa479d34150d52f7ada8063b14058
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2021-05-27 19:45:22 -0300

    Adicionados novos inputs

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "pokeLib.h"
 #include <stdlib.h>
 #include <iostream>
-#include <time.h> //clock(), CLOCKS_PER_SEC e clock_t 
+#include <time.h> //clock(), CLOCKS_PER_SEC e clock_t
 
 void *fibo(void *dta);
 void *mergeSort(void *dta);
@@ -14,81 +14,109 @@ int main()
 
     clock_t t; //variável para armazenar tempo
 
-    int escolha, termino, contador;
+    int escolha, termino, contador, tipoEscalonamento, numThreads;
     int n, *r, id;
     Atrib escalonamento;
     int *vetor = (int *)malloc((MAX + 2) * (sizeof(int)));
     vetor[0] = 0;
     vetor[1] = MAX;
-    printf("----Selecione o código----\n");
+    printf("\n----Selecione o código----\n");
     printf("- 1 - Fibonacci\n");
     printf("- 2 - Merge Sort\n");
+    printf("- 3 - Sair\n");
     printf("--------------------------\n");
     scanf("%d", &escolha);
-    t = clock(); //armazena tempo
-    switch (escolha)
+
+    while (escolha != 3)
     {
-    case 1:
-        start(4, 1);
-        //n = 3;
-        printf("Digite o Valor do Fibonacci a ser calculado: ");
-        scanf("%d", &n);
-        escalonamento.p = 0;
-        escalonamento.c = n;
-        id = spawn(&escalonamento, fibo, &n);
-        sync(id, (void **)&r);
-        finish();
-        //printf("fim");
-        printf("Fibonacci (%d) = %d \n", n, *r);
-        free(r); //adicionei essa linha
-        break;
-    case 2:
-        for (int i = 2; i < MAX + 2; i++)
+
+        printf("----Selecione o tipo de escalonamento----\n");
+        printf("- 0 - Fila (padrao)\n");
+        printf("- 1 - Pilha\n");
+        printf("-----------------------------------------\n");
+        scanf("%d", &tipoEscalonamento);
+
+        printf("----Digite a quantidade de Threads ----\n");
+        printf("-----------------------------------------\n");
+        scanf("%d", &numThreads);
+
+        if(numThreads<=0){
+            numThreads = 1;
+        }
+
+        switch (escolha)
         {
-            vetor[i] = rand() % 100;
+        case 1:
+            //n = 3;
+            printf("Digite o Valor do Fibonacci a ser calculado: ");
+            scanf("%d", &n);
+            start(numThreads, tipoEscalonamento);
+            t = clock(); //armazena tempo
+            escalonamento.p = 0;
+            escalonamento.c = n;
+            id = spawn(&escalonamento, fibo, &n);
+            sync(id, (void **)&r);
+            finish();
+            //printf("fim");
+            printf("Fibonacci (%d) = %d \n", n, *r);
+            break;
+        case 2:
+            for (int i = 2; i < MAX + 2; i++)
+            {
+                vetor[i] = rand() % 100;
+            }
+
+            for (int i = 2; i < MAX + 2; i++)
+            {
+                printf("%d, ", vetor[i]);
+            }
+            printf("\n");
+
+            start(numThreads, tipoEscalonamento);
+            escalonamento.p = 0;
+            escalonamento.c = 0;
+            t = clock();
+            id = spawn(&escalonamento, mergeSort, (void *)vetor);
+            printf("spaw ID main: %d \n", id);
+            sync(id, (void **)&r);
+            finish();
+
+            //mergeSort(vetor);
+            printf("\n\n");
+            if (MAX % 2 == 0)
+            {
+                contador = 2;
+                termino = MAX + 2;
+            }
+            else
+            {
+                contador = 3;
+                termino = MAX + 3;
+            }
+            for (contador; contador < termino; contador++)
+            {
+                printf("%d, ", vetor[contador]);
+            }
+            printf("\n");
+            free(vetor);
+            // free(r);
+            break;
+        default:
+            printf("Voce digitou errado, seu bobao :(\n");
+            break;
         }
 
-        for (int i = 2; i < MAX + 2; i++)
-        {
-            printf("%d, ", vetor[i]);
-        }
-        printf("\n");
+        free(r);                                                              //adicionei essa linha
+        t = clock() - t;                                                      //tempo final - tempo inicial
+        printf("Tempo de execucao: %lf\n", ((double)t) / ((CLOCKS_PER_SEC))); //conversão para double
 
-        start(2, 0);
-
-        escalonamento.p = 0;
-        escalonamento.c = 0;
-
-        id = spawn(&escalonamento, mergeSort, (void*)vetor);
-        printf("spaw ID main: %d \n", id);
-        sync(id, (void **)&r);
-        finish();
-
-        //mergeSort(vetor);
-        printf("\n\n");
-        if (MAX%2 ==0){
-            contador = 2;
-            termino = MAX+2;
-        }
-        else{
-            contador = 3;
-            termino = MAX +3;
-        }
-        for (contador; contador < termino; contador++)
-        {
-            printf("%d, ", vetor[contador]);
-        }
-        printf("\n");
-        free(vetor);
-       // free(r);
-        break;
-    default:
-        printf("Voce digitou errado, seu bobao :(\n");
-        break;
+        printf("\n----Selecione o código----\n");
+        printf("- 1 - Fibonacci\n");
+        printf("- 2 - Merge Sort\n");
+        printf("- 3 - Sair\n");
+        printf("--------------------------\n");
+        scanf("%d", &escolha);
     }
-
-    t = clock() - t; //tempo final - tempo inicial
-     printf("Tempo de execucao: %lf", ((double)t)/((CLOCKS_PER_SEC/1000))); //conversão para double
     return 0;
 }
 
@@ -111,7 +139,7 @@ void *fibo(void *dta)
         *n1 = (*n) - 1;
         a1.p = 0;
         a1.c = *n1;
-      //  printf("n1 %d ", *n1);
+        //  printf("n1 %d ", *n1);
         t1 = spawn(&a1, fibo, (void *)n1);
         n2 = (int *)malloc(sizeof(int));
         *n2 = *n - 2;
@@ -134,10 +162,10 @@ void *fibo(void *dta)
 
 void *mergeSort(void *dta) //int *vetor, int posicaoInicio, int posicaoFim)
 {
-    
-   // printf()
+
+    // printf()
     int *aux = (int *)dta;
-    int *vetor = &aux[2];//(int *)dta + (2 * sizeof(int));
+    int *vetor = &aux[2]; //(int *)dta + (2 * sizeof(int));
     // int *segPosicao = aux++;
     //printf("aux 0 = %d aux 1 = %d\n",aux[0],aux[1]);
     //printf("vetor = %d %d %d %d %d",vetor[0], vetor[1], vetor[2], vetor[3], vetor[4]);
@@ -147,10 +175,11 @@ void *mergeSort(void *dta) //int *vetor, int posicaoInicio, int posicaoFim)
     struct Atrib a1, a2;
     int t1, t2;
     int *r = (int *)malloc(sizeof(int));
-    *r =1;
+    *r = 1;
 
     int i, j, k, metadeTamanho, *vetorTemp;
-    if (posicaoInicio == posicaoFim){
+    if (posicaoInicio == posicaoFim)
+    {
         return (void *)1;
     }
     metadeTamanho = (posicaoInicio + (posicaoFim)) / 2;
@@ -158,14 +187,14 @@ void *mergeSort(void *dta) //int *vetor, int posicaoInicio, int posicaoFim)
     t1 = spawn(&a1, mergeSort, dta);
     sync(t1, (void **)&r);
 
-    aux[0] = metadeTamanho +1 ;
+    aux[0] = metadeTamanho + 1;
     aux[1] = posicaoFim;
     t2 = spawn(&a2, mergeSort, dta);
     sync(t2, (void **)&r);
     i = posicaoInicio;
     j = metadeTamanho + 1;
     k = 0;
-    vetorTemp = (int *)malloc(sizeof(int) * (posicaoFim - posicaoInicio +1));
+    vetorTemp = (int *)malloc(sizeof(int) * (posicaoFim - posicaoInicio + 1));
 
     while (i < metadeTamanho + 1 || j < posicaoFim + 1)
     {
@@ -177,7 +206,7 @@ void *mergeSort(void *dta) //int *vetor, int posicaoInicio, int posicaoFim)
         }
         else
         {
-            if (j == posicaoFim + 1 )
+            if (j == posicaoFim + 1)
             {
                 vetorTemp[k] = vetor[i];
                 i++;
@@ -201,8 +230,7 @@ void *mergeSort(void *dta) //int *vetor, int posicaoInicio, int posicaoFim)
         }
     }
 
-
-    for (i = posicaoInicio; i <= posicaoFim; i++)//Provável problema
+    for (i = posicaoInicio; i <= posicaoFim; i++) //Provável problema
     {
         vetor[i] = vetorTemp[i - posicaoInicio];
     }
